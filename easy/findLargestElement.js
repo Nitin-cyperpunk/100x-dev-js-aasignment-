a35f9233905cdcc0b1da919136311eb2d6a44a60
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2024-11-05 23:55:29 +0530

    second commit

--- easy/findLargestElement.js
+++ easy/findLargestElement.js
@@ -6,7 +6,17 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let max = numbers[0];  // Initialize max with the first element of the array
+
+    for (let i = 1; i < numbers.length; i++) { 
+        if (numbers[i] > max) {  
+            max = numbers[i];  
+        }
+    }
+
+    return max;  
 }
+
+console.log(findLargestElement([3, 9, 2, 11, 1]));  
 
 module.exports = findLargestElement;
